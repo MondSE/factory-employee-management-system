622519a078887c747ed508429ead327ec4e41cef
Almond Tuazon <azirtuazon@gmail.com>
2026-06-02 20:07:39 +0800

    Database: Requests Validation

---
+++ app/Http/Requests/StoreFactoryRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreFactoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'factory_id' => 'required|exists:factories,id',
+            'email' => 'nullable|email',
+            'phone' => 'nullable',
+        ];
+    }
+}
